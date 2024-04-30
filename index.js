cf2fe2ab362167dfd60cf2afed8b50c69a0fc35c
Cedric Gio Manuel <113290298+CedricManuel02@users.noreply.github.com>
2024-04-30 14:22:47 +0800

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 require("dotenv").config()
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(cors())
